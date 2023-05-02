fd7f1b32cee70400a82ac58bf655ad07a5bc5ed3
leonardo <leon_vieira@live.com>
2023-05-02 17:41:42 -0300

    Condição If

--- Aula C-Sharp/Variaveis/Program.cs
+++ Aula C-Sharp/Variaveis/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bem vindo ao Show");
+            Console.WriteLine("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Sua idade permite entrar no show, seja bem vindo " + nome + "!");
+            }
+            else
+            {
+                Console.WriteLine("Você esta acompanhado por um adulto?");
+                string acompanhado = Console.ReadLine();
+
+                if (acompanhado == "sim")
+                {
+                    Console.WriteLine("Como voce esta acompanhado, pode entrar!");
+                }
+                else
+                {
+                    Console.WriteLine("Que pena " + nome + ", infelizmente não será possivel entrar no show.");
+                }
+            }
+
         }
     }
 }
